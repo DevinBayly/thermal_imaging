31448ba005f5b3c0e5bf46bbfb768f185a95ab3b
Devin Richard Bayly <baylyd@cpu38.elgato.hpc.arizona.edu>
2022-01-10 16:58:07 -0700

    latest on hpc

--- test_classes.py
+++ test_classes.py
@@ -1,4 +1,5 @@
 
 from classes import *
-##timer("outer timer",test_video,SimplestPass)
+#timer("outer timer",test_video,SimplestPass)
 timer("parallel timer",test_parallel_video,OnlyDetect)
+##test_detect()
