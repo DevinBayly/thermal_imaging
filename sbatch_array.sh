33bc2672d2d183017ff5f9e8080b87ba8a9bda74
Devin Bayly <baylyd@email.arizona.edu>
2022-02-08 14:20:26 -0700

    set it for bjr

--- sbatch_array.sh
+++ sbatch_array.sh
@@ -5,10 +5,9 @@
 #SBATCH --mem=5gb                    
 #SBATCH --time=01:30:00   
 #SBATCH --partition=standard
-#SBATCH --account=visteam   
+#SBATCH --account=bjr   
 #SBATCH --array 0-5
  
 # SLURM Inherits your environment. cd $SLURM_SUBMIT_DIR not needed
 pwd; hostname; date
-cd /groups/chrisreidy/baylyd/thermal_imaging/ 
 singularity exec thermal_imaging.sif bash process_vids.sh $1 $2 ${SLURM_ARRAY_TASK_ID}
