24356d7749a7ca9a267649d9fbf7fa3e741ddcfc
Devin Bayly <baylyd@email.arizona.edu>
2022-07-19 13:10:32 -0700

    changing some of the parameters

--- sbatch_array.sh
+++ sbatch_array.sh
@@ -2,11 +2,11 @@
 #SBATCH --output=Sample_SLURM_Job-%a.out
 #SBATCH --ntasks=16
 #SBATCH --nodes=1             
-#SBATCH --mem=5gb                    
-#SBATCH --time=01:30:00   
+#SBATCH --mem=32gb                    
+#SBATCH --time=03:30:00   
 #SBATCH --partition=standard
 #SBATCH --account=bjr   
-#SBATCH --array 0-5
+#SBATCH --array 0-63
  
 # SLURM Inherits your environment. cd $SLURM_SUBMIT_DIR not needed
 pwd; hostname; date
